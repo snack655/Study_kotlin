945cade7db4dd971a601fe90347585cd49ba0167
snack655 <80940200+snack655@users.noreply.github.com>
2022-02-14 18:43:45 +0900

    Study : Lambda and DSL
    2022-02-14
    Make simple DSL

---
+++ StudyStandardFunAndInOutput/src/main/kotlin/lambdaAndDSL/PersonDSL.kt
@@ -0,0 +1,31 @@
+package lambdaAndDSL
+
+data class Person(
+    var name: String? = null,
+    var age: Int? = null,
+    var job: Job? = null)
+
+data class Job(
+    var category: String? = null,
+    var position: String? = null,
+    var extension: Int? = null)
+
+fun person(block: Person.() -> Unit): Person = Person().apply(block)
+
+fun Person.job(block: Job.() -> Unit) {
+    job = Job().apply(block)
+}
+
+fun main() {
+    val person = person {
+        name = "Kildong"
+        age = 40
+        job {
+            category = "IT"
+            position = "Android Developer"
+            extension = 1234
+        }
+    }
+    println(person)
+}
+
